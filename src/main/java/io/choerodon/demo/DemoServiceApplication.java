ee464125b87439eff7e403c218d9054aaf3f6263
azengqiang <qiang.zeng@hand-china.com>
2019-07-04 11:51:17 +0800

    [UPD] 更新微服务应用模板

--- src/main/java/io/choerodon/demo/DemoServiceApplication.java
+++ src/main/java/io/choerodon/demo/DemoServiceApplication.java
@@ -3,7 +3,7 @@ package io.choerodon.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 
@@ -12,7 +12,7 @@ import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 @EnableEurekaClient
 @EnableChoerodonResourceServer
 public class DemoServiceApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(DemoServiceApplication.class, args);
     }
 }
